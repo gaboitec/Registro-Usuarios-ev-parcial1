80baa5cc78bbffd7830c285133eca0a047729609
Yefry Gabriel García Caxaj <caxajgabriel2@gmail.com>
2025-07-28 01:14:32 -0600

    clases

--- clases.py
+++ clases.py
@@ -1,2 +1,20 @@
 class Empleado:
-    def __
+    def __init__(self, codigo, nombre, departamento, antiguedad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.departamento = departamento
+        self.antiguedad = antiguedad
+
+
+class Evaluacion:
+    def __init__(self, puntualidad, equipo, productividad, observacion, estado):
+        self.puntualidad = puntualidad
+        self.equipo = equipo
+        self.productividad = productividad
+        self.observacion = observacion
+        self.estado = estado
+
+class Contacto:
+    def __init__(self, telefono, correo):
+        self.telefono = telefono
+        self.correo = correo
